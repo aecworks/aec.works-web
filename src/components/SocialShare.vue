7035da6d70560bdd5fb062f76af3ce5cca2eb1a7
Gui Talarico <gtalarico@users.noreply.github.com>
2020-09-17 00:25:43 -0700

    Added Social Share (#5)

---
+++ src/components/SocialShare.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <LinkedInShare button_design="outline" has_square_edges has_icon :page_url="pageUrl" />
+    <TwitterShare button_design="outline" has_square_edges has_icon :page_url="pageUrl" />
+  </div>
+</template>
+
+<script>
+import LinkedInShare from 'vue-goodshare/src/providers/LinkedIn.vue'
+import TwitterShare from 'vue-goodshare/src/providers/Twitter.vue'
+
+export default {
+  name: 'SocialShare',
+  components: { LinkedInShare, TwitterShare },
+  props: ['pageUrl'],
+  data() {
+    return {}
+  },
+  computed: {},
+}
+</script>
+
+<style lang="scss" scoped>
+// Todo
+</style>
