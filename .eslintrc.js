2869e594a35b94f2e5770f98b3785f3bb08c3a70
Daniel da Rocha <danrocha@gmail.com>
2020-09-25 17:30:31 +0200

    chore: change no-console rule to warning

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,8 +5,8 @@ module.exports = {
   },
   extends: ['eslint:recommended', 'plugin:vue/recommended', '@vue/prettier'],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
   parserOptions: {
     parser: 'babel-eslint',
