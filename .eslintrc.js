ea071864258b5e570e7656e7e2dee94562f0b654
Daniel da Rocha <danrocha@gmail.com>
2020-09-28 09:01:12 +0200

    test: add jest and cypress plugins for testing

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,27 @@
 module.exports = {
   root: true,
+
   env: {
     node: true,
   },
+
   extends: ['eslint:recommended', 'plugin:vue/recommended', '@vue/prettier'],
+
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
+
   parserOptions: {
     parser: 'babel-eslint',
   },
+
+  overrides: [
+    {
+      files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
+      env: {
+        jest: true,
+      },
+    },
+  ],
 }
