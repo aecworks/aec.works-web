3e1b7782b9886fa1486e83f7cc809e2e9a2f2d1e
Daniel da Rocha <danrocha@gmail.com>
2020-09-30 14:50:19 +0200

    feat: implement LazyImg component

---
+++ src/components/LazyImg.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-lazy-image :src="src" :src-placeholder="require('@/assets/images/spinner.svg')" />
+</template>
+
+<script>
+import VLazyImage from 'v-lazy-image'
+
+export default {
+  components: {
+    VLazyImage,
+  },
+  props: {
+    src: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
