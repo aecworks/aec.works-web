57676de022b540232282b1de54f1c492f82ee67d
gtalarico <gtalarico.dev@gmail.com>
2020-10-29 23:41:20 -0300

    Tweaker animation

--- src/components/SocialIcons.vue
+++ src/components/SocialIcons.vue
@@ -1,17 +1,23 @@
 <template>
   <div id="social">
-    <a href="https://github.com/aecworks" class="transition social-icon--container">
-      <img alt="Github" src="@/assets/images/social-icons/github.svg" />
+    <a href="https://github.com/aecworks" class="social-icon--container">
+      <div class="social-icon--icon">
+        <img alt="Github" src="@/assets/images/social-icons/github.svg" />
+      </div>
       <div class="social-icon--text">Github</div>
     </a>
-    <a href="https://twitter.com/gtalarico" class="transition social-icon--container">
-      <img alt="Twitter" src="@/assets/images/social-icons/twitter.svg" />
+    <a href="https://twitter.com/aec_works" class="social-icon--container">
+      <div class="social-icon--icon">
+        <img alt="Twitter" src="@/assets/images/social-icons/twitter.svg" />
+      </div>
       <div class="social-icon--text">Twitter</div>
     </a>
-    <a href="https://twitter.com/gtalarico" class="transition social-icon--container">
-      <img alt="About" src="@/assets/images/social-icons/about.svg" />
+    <!-- <a href="/about" class="social-icon--container">
+      <div class="social-icon--icon">
+        <img alt="About" src="@/assets/images/social-icons/about.svg" />
+      </div>
       <div class="social-icon--text">About</div>
-    </a>
+    </a> -->
   </div>
 </template>
 
@@ -26,12 +32,12 @@ export default {
   z-index: 2;
   position: fixed;
   display: flex;
+  align-items: flex-end;
   @include for-large-up {
     right: 0;
     top: 50vh;
     margin-right: 1rem;
     flex-direction: column;
-    direction: rtl;
   }
   @include for-large-down {
     top: calc(100vh - 32px - 1rem);
@@ -39,47 +45,40 @@ export default {
   }
 }
 
-.transition {
-  width: 32px;
-}
-
-.transition:hover {
-  width: 120px;
-  transition: all 200ms;
-}
-
 .social-icon--container {
-  direction: ltr;
   align-items: center;
-  justify-content: center;
   background-color: #fff;
   display: flex;
   @extend .border-thin;
-  border-radius: 2px;
-  height: 32px;
   margin: 0.25rem 0;
 
   @include for-large-down {
-    width: 32px;
     margin: 0 0.25rem;
     @include shadow-color($dark);
   }
 
-  & .social-icon--text {
-    font-family: $font-family;
-    font-weight: $font-weight-bold;
-    display: none;
-  }
+  overflow-y: hidden;
+  height: 32px;
+  max-width: 32px;
+  transition: max-width 200ms ease-in-out;
 
   &:hover {
-    @include shadow-color($dark);
-    padding: 0 0.25rem;
-    & .social-icon--text {
-      font-family: $font-family;
-      font-weight: $font-weight-bold;
-      display: block;
-      margin-left: 1rem;
-    }
+    max-width: 100px;
+  }
+
+  .social-icon--icon {
+    flex-shrink: 0;
+    width: 24px;
+    height: 24px;
+    padding: 2px 4px;
+    text-align: center;
+  }
+
+  .social-icon--text {
+    align-self: space-between;
+    font-family: $font-family;
+    font-weight: $font-weight-bold;
+    padding: 0 8px;
   }
 }
 </style>
