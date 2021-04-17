b89e8b07ad3bef416d305dbaf209ebff6a648012
Daniel da Rocha <daniel.darocha@havr.io>
2021-04-17 17:18:36 +0200

    implement about page

---
+++ src/views/About.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="tos-container">
+    <div class="tos-header">
+      <h1 class="tos-title">About</h1>
+      <div class="tos-marker"></div>
+    </div>
+    <div class="mt-3">
+      <p>
+        <strong>aec.works</strong>
+        is a curated list of kick-ass product-oriented AEC companies and startups.
+      </p>
+      <h2 class="mb-1 mt-3">History</h2>
+      <p>
+        In 2019, I started a small website called AecStartups.com started as a spreadsheet to track
+        what he thought were cool new companies springing up around AEC. The project is still live
+        at
+        <a href="http://aecstartups.com">aecstartups.com</a>
+        .
+      </p>
+      <p>
+        As the word went around, it started to get contributions from visitors. But the experience
+        to view or collaborate was not great, and there was also no way to track/moderate changes to
+        improve content over time. It became a time-consuming task to moderate submissions and
+        format logos properly.
+      </p>
+      <p>
+        <strong>aec.works</strong>
+        started as a redo of AecStartups, with a few changes:
+      </p>
+      <ul class="tos-list">
+        <li class="ml-1">
+          <p>
+            • It is about
+            <strong>all of AEC</strong>
+            and not just startups
+          </p>
+        </li>
+        <li class="ml-1"><p>• It invites collaboration and has tools to manage content</p></li>
+        <li class="ml-1"><p>• It allows for discussion and user contributions</p></li>
+      </ul>
+
+      <p>
+        After speaking with many people in the field, I got together with a few equally motivated
+        people to finalize and build the version you are seeing now.
+      </p>
+
+      <h2 class="mb-1 mt-3">Building a community</h2>
+
+      <p>
+        I am deeply inspired and grateful for all the communities that spring up around different
+        expertise and topics: Maxwell Render Forum, Grasshopper, Sketchup Forum, Stack Overflow,
+        Dynamo Forum, GitHub, and Slack Channels like python-aec or Architechie, just to name a few.
+        I attribute many of the skills I have acquired over the few years to the knowledge and
+        support these communities offer.
+      </p>
+
+      <p>
+        Although these technical forums are great, I believe there is value in bringing people
+        across the industry to a single place to exchange content and ideas. Generic social
+        platforms like Twitter and LinkedIn are not tailored for AEC, and content gets mixed with
+        and diluted by other unrelated topics and industries.
+      </p>
+
+      <p>
+        Social platforms are ultimately all about the people and the community that forms around
+        them. Our goal with aec.works is to stay simple and bring lots of energy and great content
+        to everyone.
+      </p>
+
+      <h2 class="mb-1 mt-3">Vision</h2>
+
+      <p>Text about vision.</p>
+
+      <h2 class="mb-1 mt-3">People</h2>
+
+      <p>Talking about who you are and who we are, with names and photos!!</p>
+      <p>And never forget Justin!!</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  metaInfo: {
+    title: 'About',
+  },
+}
+</script>
