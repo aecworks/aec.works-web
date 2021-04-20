7404198d5e7d2ce372c0c23964d227b9eb33e425
Daniel da Rocha <danrocha@gmail.com>
2021-04-20 08:13:31 +0200

    refactor: implement sorting component
    - Move Sorting to its own component
    - Add sorted & reverse arrows to each item

--- src/views/CompanyList.vue
+++ src/views/CompanyList.vue
@@ -32,21 +32,7 @@
 
       <!-- Sorting -->
       <div class="mt-1 mb-2">
-        <label class="mt-1">Sorting</label>
-        <div class="sorting-options fill-x">
-          {{ $route.query.reverse ? '🔻' : '' }}
-          <ul>
-            <li
-              v-for="paramName in sortingOptions"
-              :key="paramName"
-              class="pointer small"
-              :class="getSortStyle(paramName)"
-              @click="handleSortChange(paramName)"
-            >
-              {{ paramName }}
-            </li>
-          </ul>
-        </div>
+        <Sorting @sort="refetch" />
       </div>
 
       <p class="mt-3 small">
@@ -73,6 +59,7 @@ import Button from '../components/forms/Button.vue'
 import Pagination from '../components/Pagination.vue'
 import HashtagInput from '../components/forms/HashtagInput.vue'
 import Loader from '../components/Loader.vue'
+import Sorting from '../components/Sorting.vue'
 import TextInput from '../components/forms/TextInput.vue'
 import api from '@/api'
 import CompanyCard from '@/components/CompanyCard'
@@ -88,6 +75,7 @@ export default {
   components: {
     CompanyCard,
     TextInput,
+    Sorting,
     Loader,
     HashtagInput,
     Button,
@@ -105,7 +93,6 @@ export default {
   },
   data() {
     return {
-      sortingOptions: ['claps', 'name', 'updated'],
       PAGE_SIZE: 10,
       numPages: 1,
       hasMore: false,
@@ -173,26 +160,6 @@ export default {
       this.refetch()
     }, 200),
 
-    getSortStyle(sortBy) {
-      let isActive = this.$route.query.sort == sortBy
-      let isReverse = this.$route.query.reverse
-      return (isActive ? 'active' : '') + ' ' + (isActive && isReverse ? 'reversed' : '')
-    },
-    handleSortChange(sortBy) {
-      let isAlready = Boolean(this.$route.query.sort == sortBy)
-      let isReversed = Boolean(this.$route.query.reverse)
-      if (isAlready && !isReversed) {
-        this.$router.replace({ query: { ...this.$route.query, reverse: 1 } }).catch(() => {})
-      } else if (isAlready && isReversed) {
-        popQuery(this.$router, this.$route.query, 'reverse')
-      } else {
-        this.$router.replace({ query: { ...this.$route.query, sort: sortBy } }).catch(() => {})
-        if (isReversed) {
-          popQuery(this.$router, this.$route.query, 'reverse')
-        }
-      }
-      this.refetch()
-    },
     handleHashtagFilterChanged(tags) {
       if (tags.length) {
         const hashtagStr = tags.join(',')
@@ -226,24 +193,6 @@ export default {
 .sidebar {
   @include for-large-up {
     text-align: right;
-  }
-}
-.sorting-options {
-  display: flex;
-  @include for-large-up {
-    justify-content: flex-end;
-  }
-  li {
-    text-decoration: underline;
-    text-decoration-color: $yellow;
-    &:not(:last-child) {
-      margin-right: 1rem;
-    }
-    display: inline-block;
-    color: $light-gray;
-    &.active {
-      color: $dark;
-    }
   }
 }
 </style>
